beb482d3427eab562ee6d17e3b5df74b34730db5
HLCFIIT01 <HLCFIIT01@X-HLCFIIT01>
2021-12-02 11:09:57 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Yung_Ching_Rehouse.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
